3eedd934fd707d38e503fdaacf9ba2b368b8fd3a
vbalaji215-dev <vbalaji215@outlook.com>
2025-04-20 18:05:43 -0400

    Update prior to deploy

--- src/test/java/com/brihaspathee/artemis/ApiGatewayApplicationTests.java
+++ src/test/java/com/brihaspathee/artemis/ApiGatewayApplicationTests.java
@@ -2,9 +2,28 @@ package com.brihaspathee.artemis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ApiGatewayApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("artemis_gatewaydb")
+            .withUsername("artemis_gateway_admin")
+            .withPassword("password");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
